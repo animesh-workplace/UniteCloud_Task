f4441e7152f2d0a243aac05873d4f1e706197121
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-01-20 00:42:43 +0530

    added file - pages/index.vue

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<SectionHero />
+		<SectionTrusted />
+		<SectionLifecycle />
+		<SectionHeal />
+		<SectionWellness />
+		<SectionHealthplan />
+		<SectionWork />
+		<SectionOffer />
+		<SectionDashboard />
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
