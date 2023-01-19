7d2055b3a4fcd61d707e408fef6b7be643667ca8
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-01-20 00:42:42 +0530

    added file - layouts/default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<CommonNavBar />
+		<main class="flex bg-gray-100 min-h-screen">
+			<Nuxt class="w-full mt-16" />
+		</main>
+		<CommonFooter />
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+<style></style>
