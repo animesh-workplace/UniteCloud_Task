facc601be0b13608cb664bb77275be57c7b02693
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-01-20 00:42:40 +0530

    added file - components/Section/Hero.vue

---
+++ components/Section/Hero.vue
@@ -0,0 +1,37 @@
+<template>
+	<section class="bg-white">
+		<div
+			class="grid grid-cols-12 gap-4 px-24 py-64 bg-[url('@/assets/images/hero.png')] bg-contain bg-no-repeat bg-right"
+		>
+			<div class="col-span-8">
+				<div class="mr-auto place-self-center lg:col-span-4 mb-16">
+					<h1 class="text-4xl font-normal tracking-wide leading-none text-[#284948]">
+						LOOK AFTER THE PEOPLE,<br />WHO LOOK AFTER YOUR BUSINESS
+					</h1>
+				</div>
+				<h1 class="text-4xl font-semibold tracking-wide leading-none text-[#284948] mb-12">
+					HEALTH WELLNESS
+				</h1>
+				<button
+					type="button"
+					class="text-white text-semibold bg-[#F28737] rounded-lg text-lg px-5 py-2.5 text-center mr-3 md:mr-0"
+				>
+					REQUEST DEMO
+				</button>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
