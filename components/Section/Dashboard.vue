5377830973aa9f61b4dfe89d595193140a91b6f3
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-01-20 00:42:38 +0530

    added file - components/Section/Dashboard.vue

---
+++ components/Section/Dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+	<section class="bg-[#FBFEE9] pb-24">
+		<div class="bg-gradient-to-l from-[#141414] to-[#284948] p-28"></div>
+		<div class="grid grid-cols-2 gap-4 content-center items-start px-24 pt-24 -mt-72">
+			<div>
+				<img alt="Icon2" class="mx-auto" src="@/assets/images/dashboard.png" />
+			</div>
+			<div>
+				<div>
+					<h2 class="text-[36px] font-bold uppercase text-white">COMPLETE DASHBOARD FOR YOUR COMPANY</h2>
+					<button
+						type="button"
+						class="text-white text-semibold bg-[#F28737] rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 mt-4"
+					>
+						REQUEST DEMO
+					</button>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
