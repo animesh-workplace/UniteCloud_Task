3a9d2a85dd8903e2fb0b30f8e299be1bc0637db3
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-01-20 00:42:41 +0530

    added file - components/Section/Trusted.vue

---
+++ components/Section/Trusted.vue
@@ -0,0 +1,24 @@
+<template>
+	<section class="bg-[#FBFEE9] px-12 py-24">
+		<h2 class="text-[28px] font-extrabold uppercase text-center mb-8 text-[#284948]">Trusted by many</h2>
+		<div class="grid grid-cols-12 gap-20">
+			<div data-aos="fade-up" class="col-span-3 bg-[#284948] p-16 rounded"></div>
+			<div data-aos="fade-up" class="col-span-3 bg-[#DD7A1D] p-16 rounded"></div>
+			<div data-aos="fade-up" class="col-span-3 bg-[#3E8C58] p-16 rounded"></div>
+			<div data-aos="fade-up" class="col-span-3 bg-[#41345F] p-16 rounded"></div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
