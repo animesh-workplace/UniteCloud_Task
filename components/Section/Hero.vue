52bfabaf6200088a946ee3ae268d15b2c3f733ef
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-01-20 10:45:24 +0530

    added responsiveness to hero

--- components/Section/Hero.vue
+++ components/Section/Hero.vue
@@ -1,23 +1,23 @@
 <template>
 	<section class="bg-white">
 		<div
-			class="grid grid-cols-12 gap-4 px-24 py-64 bg-[url('@/assets/images/hero.png')] bg-contain bg-no-repeat bg-right"
+			class="grid grid-cols-12 gap-4 px-8 py-12 md:px-24 bg-white md:py-64 md:bg-[url('@/assets/images/hero.png')] md:bg-contain md:bg-no-repeat md:bg-right place-content-center"
 		>
-			<div class="col-span-8">
-				<div class="mr-auto place-self-center lg:col-span-4 mb-16">
-					<h1 class="text-4xl font-normal tracking-wide leading-none text-[#284948]">
+			<div class="md:col-span-8 col-span-12">
+				<div class="text-center md:text-left">
+					<h1 class="text-2xl md:text-4xl font-normal tracking-wide leading-none text-[#284948] mb-16">
 						LOOK AFTER THE PEOPLE,<br />WHO LOOK AFTER YOUR BUSINESS
 					</h1>
+					<h1 class="text-2xl md:text-4xl font-semibold tracking-wide leading-none text-[#284948] mb-12">
+						HEALTH WELLNESS
+					</h1>
+					<button
+						type="button"
+						class="text-white text-semibold bg-[#F28737] rounded-lg text-lg px-5 py-2.5 text-center"
+					>
+						REQUEST DEMO
+					</button>
 				</div>
-				<h1 class="text-4xl font-semibold tracking-wide leading-none text-[#284948] mb-12">
-					HEALTH WELLNESS
-				</h1>
-				<button
-					type="button"
-					class="text-white text-semibold bg-[#F28737] rounded-lg text-lg px-5 py-2.5 text-center mr-3 md:mr-0"
-				>
-					REQUEST DEMO
-				</button>
 			</div>
 		</div>
 	</section>
