0296aa2fae86478a66430d92df5a72ec1fda662d
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-01-20 10:45:36 +0530

    added responsiveness to offer

--- components/Section/Offer.vue
+++ components/Section/Offer.vue
@@ -3,9 +3,9 @@
 		<h2 class="text-[36px] font-bold uppercase text-center text-[#284948] mb-16">WHAT WE OFFER</h2>
 
 		<div class="container mx-auto">
-			<div class="grid grid-cols-4 gap-20 content-center items-center px-44">
+			<div class="grid grid-cols-3 md:grid-cols-4 gap-20 content-center items-center px-auto md:px-44">
 				<div v-for="(item, index) in icon_items" :key="index">
-					<div class="flex items-center">
+					<div class="flex items-center" data-aos="fade-up">
 						<div class="w-20 h-20 rounded bg-[#D2E289] p-4 inline-block mr-2">
 							<img src="@/assets/images/offer-icon1.png" alt="Icon1" v-if="item.icon == 1" />
 							<img src="@/assets/images/offer-icon2.png" alt="Icon1" v-if="item.icon == 2" />
@@ -13,8 +13,6 @@
 						</div>
 						<h4 class="inline-block font-medium text-center">{{ item.content }}</h4>
 					</div>
-					<!-- <div class="px-4 mt-8"> -->
-					<!-- </div> -->
 				</div>
 			</div>
 		</div>
