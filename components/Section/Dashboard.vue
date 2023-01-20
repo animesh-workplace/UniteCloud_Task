fd1cbb9a7ce6c7c31fed4e2bd7cbe8a5fb431667
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-01-20 10:44:58 +0530

    added responsiveness to dashboard

--- components/Section/Dashboard.vue
+++ components/Section/Dashboard.vue
@@ -1,13 +1,15 @@
 <template>
 	<section class="bg-[#FBFEE9] pb-24">
 		<div class="bg-gradient-to-l from-[#141414] to-[#284948] p-28"></div>
-		<div class="grid grid-cols-2 gap-4 content-center items-start px-24 pt-24 -mt-72">
+		<div class="grid grid-cols-2 gap-4 content-center items-start px-24 pt-20 md:pt-24 md:-mt-72 -mt-56">
 			<div>
 				<img alt="Icon2" class="mx-auto" src="@/assets/images/dashboard.png" />
 			</div>
 			<div>
 				<div>
-					<h2 class="text-[36px] font-bold uppercase text-white">COMPLETE DASHBOARD FOR YOUR COMPANY</h2>
+					<h2 class="text-[20px] md:text-[36px] font-bold uppercase text-white">
+						COMPLETE DASHBOARD FOR YOUR COMPANY
+					</h2>
 					<button
 						type="button"
 						class="text-white text-semibold bg-[#F28737] rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 mt-4"
