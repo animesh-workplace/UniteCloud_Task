a409fba78b301fbd83ee419b3a3970cbc84f7eef
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-01-20 10:45:19 +0530

    added responsiveness to healthplan

--- components/Section/Healthplan.vue
+++ components/Section/Healthplan.vue
@@ -1,20 +1,20 @@
 <template>
-	<section class="bg-[#FBFEE9] py-24">
+	<section class="bg-[#FBFEE9] py-8 md:py-24">
 		<h2 class="text-[26px] font-bold uppercase text-[#284948] text-center mb-16">HEALTH PLAN</h2>
-		<div class="grid grid-cols-12 gap-0 p-42">
+		<div class="grid grid-cols-12 gap-0" data-aos="fade-up">
 			<div class="col-span-6 indicator max-h-96">
-				<div class="bg-[#284948] p-12">
+				<div class="bg-[#284948] p-6 md:p-12">
 					<h2 class="text-[26px] font-bold uppercase text-[#C3D797] pl-5">HEAL</h2>
-					<ul class="list-disc pl-10 text-white font-medium mt-6 px-16">
-						<li class="mb-8">
+					<ul class="list-disc pl-10 text-white md:text-base text-sm font-medium mt-6 px-2 md:px-16">
+						<li class="mb-4 md:mb-8">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
 							consectetur adipiscing elit.
 						</li>
-						<li class="mb-8">
+						<li class="mb-4 md:mb-8">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
 							consectetur adipiscing elit.
 						</li>
-						<li class="mb-8">
+						<li class="mb-0 md:mb-4">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
 							consectetur adipiscing elit.
 						</li>
@@ -26,23 +26,23 @@
 			</div>
 		</div>
 
-		<div class="grid grid-cols-12 gap-0 p-42">
-			<div class="col-span-6 indicator max-h-96">
+		<div class="grid grid-cols-12 gap-0" data-aos="fade-up">
+			<div class="col-span-6 max-h-96 indicator">
 				<div class="h-96 bg-[url(@/assets/images/health-image2.png)] bg-no-repeat bg-cover"></div>
 			</div>
-			<div class="col-span-6 max-h-6">
-				<div class="bg-[#284948] p-12">
+			<div class="col-span-6 max-h-96">
+				<div class="bg-[#284948] p-6 md:p-12">
 					<h2 class="text-[26px] font-bold uppercase text-[#C3D797] pl-5">HEAL</h2>
-					<ul class="list-disc pl-10 text-white font-medium mt-6 px-16">
-						<li class="mb-8">
+					<ul class="list-disc pl-10 text-white md:text-base text-sm font-medium mt-6 px-2 md:px-16">
+						<li class="mb-4 md:mb-8">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
 							consectetur adipiscing elit.
 						</li>
-						<li class="mb-8">
+						<li class="mb-4 md:mb-8">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
 							consectetur adipiscing elit.
 						</li>
-						<li class="mb-8">
+						<li class="mb-0 md:mb-[1.1rem]">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
 							consectetur adipiscing elit.
 						</li>
@@ -51,20 +51,20 @@
 			</div>
 		</div>
 
-		<div class="grid grid-cols-12 gap-0 p-42">
+		<div class="grid grid-cols-12 gap-0" data-aos="fade-up">
 			<div class="col-span-6 indicator max-h-96">
-				<div class="bg-[#284948] p-12">
+				<div class="bg-[#284948] p-6 md:p-12">
 					<h2 class="text-[26px] font-bold uppercase text-[#C3D797] pl-5">HEAL</h2>
-					<ul class="list-disc pl-10 text-white font-medium mt-6 px-16">
-						<li class="mb-8">
+					<ul class="list-disc pl-10 text-white md:text-base text-sm font-medium mt-6 px-2 md:px-16">
+						<li class="mb-4 md:mb-8">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
 							consectetur adipiscing elit.
 						</li>
-						<li class="mb-8">
+						<li class="mb-4 md:mb-8">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
 							consectetur adipiscing elit.
 						</li>
-						<li class="mb-4">
+						<li class="mb-0 md:mb-[1.1rem]">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
 							consectetur adipiscing elit.
 						</li>
@@ -105,9 +105,9 @@ export default {
 	content: url('@/assets/images/healthplan-icon.png');
 	box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
 }
-.img {
-	width: 100vw;
-	height: 100vh;
-	object-fit: cover;
+@media (max-width: 768px) {
+	.indicator::after {
+		transform: scale(0.7);
+	}
 }
 </style>
