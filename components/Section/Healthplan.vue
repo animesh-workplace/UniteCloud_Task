24d97162812e3ed116d33fdee9a54af0733ca033
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-01-27 16:18:44 +0530

    Responsive: Grid column changes to 12 col on xl-device and the indicator becomes hidden until xl-device

--- components/Section/Healthplan.vue
+++ components/Section/Healthplan.vue
@@ -1,7 +1,7 @@
 <template>
 	<section class="bg-[#FBFEE9] py-8 md:py-24">
 		<h2 class="text-[26px] font-bold uppercase text-[#284948] text-center mb-16">HEALTH PLAN</h2>
-		<div class="grid grid-cols-12 gap-0" data-aos="fade-up">
+		<div class="grid grid-cols-6 xl:grid-cols-12 gap-0" data-aos="fade-up">
 			<div class="col-span-6 indicator max-h-96">
 				<div class="bg-[#284948] p-6 md:p-12">
 					<h2 class="text-[26px] font-bold uppercase text-[#C3D797] pl-5">HEAL</h2>
@@ -26,11 +26,11 @@
 			</div>
 		</div>
 
-		<div class="grid grid-cols-12 gap-0" data-aos="fade-up">
+		<div class="grid grid-cols-6 xl:grid-cols-12 gap-0" data-aos="fade-up">
 			<div class="col-span-6 max-h-96 indicator">
 				<div class="h-96 bg-[url(@/assets/images/health-image2.png)] bg-no-repeat bg-cover"></div>
 			</div>
-			<div class="col-span-6 max-h-96">
+			<div class="col-span-6 max-h-96 order-first xl:order-none">
 				<div class="bg-[#284948] p-6 md:p-12">
 					<h2 class="text-[26px] font-bold uppercase text-[#C3D797] pl-5">HEAL</h2>
 					<ul class="list-disc pl-10 text-white md:text-base text-sm font-medium mt-6 px-2 md:px-16">
@@ -51,7 +51,7 @@
 			</div>
 		</div>
 
-		<div class="grid grid-cols-12 gap-0" data-aos="fade-up">
+		<div class="grid grid-cols-6 xl:grid-cols-12 gap-0" data-aos="fade-up">
 			<div class="col-span-6 indicator max-h-96">
 				<div class="bg-[#284948] p-6 md:p-12">
 					<h2 class="text-[26px] font-bold uppercase text-[#C3D797] pl-5">HEAL</h2>
@@ -105,9 +105,10 @@ export default {
 	content: url('@/assets/images/healthplan-icon.png');
 	box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
 }
-@media (max-width: 768px) {
+@media (max-width: 1280px) {
 	.indicator::after {
 		transform: scale(0.7);
+		display: none;
 	}
 }
 </style>
